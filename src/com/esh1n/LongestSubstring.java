c005c57a754139fc71954176eb871e8028758d7a
Sergey Eshin <eshinsergey@gmail.com>
2019-08-02 16:29:08 +0300

    fixed longest substring with unrepeatable chars

--- src/com/esh1n/LongestSubstring.java
+++ src/com/esh1n/LongestSubstring.java
@@ -6,30 +6,34 @@ import java.util.Set;
 public class LongestSubstring {
 
     public static void main(String[] args) {
-	  String firstRow = "abcabcbb";
-	  String secondRow = "bbbbb";
-	  String thirdRow = "pwwkew";
+	String firstRow = "abcabcbb";
+	String secondRow = "bbbbb";
+	String thirdRow = "pwwkew";
         String emptyRow = "abcdbcd";
         System.out.println("count 1:" + lengthOfLongestSubstring(emptyRow));
-	  System.out.println("count 1:" + lengthOfLongestSubstring(firstRow));
+	System.out.println("count 1:" + lengthOfLongestSubstring(firstRow));
         System.out.println("count 2:" + lengthOfLongestSubstring(secondRow));
         System.out.println("count 3:" + lengthOfLongestSubstring(thirdRow));
     }
 
-    public static int lengthOfLongestSubstring(String s) {
+        public int lengthOfLongestSubstring(String s) {
         int n = s.length();
+        int ans = 0;
+        for(int i = 0;i<n-1;i++)
+            for(int j = i+1;j<=n;j++){
+                if(allUnique(s,i,j)) ans = Math.max(ans, j-i);
+            }
+         return ans;
+                   
+    }
+    
+    public boolean allUnique(String s,int start, int end){
         Set<Character> set = new HashSet<>();
-        int ans = 0, i = 0, j = 0;
-        while (i < n && j < n) {
-            // try to extend the range [i, j]
-            if (!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
-                ans = Math.max(ans, j - i);
-            }
-            else {
-                set.remove(s.charAt(i++));
-            }
+        for(int i = start; i< end;i++){
+            Character ch = s.charAt(i);
+            if(set.contains(ch)) return false;
+            set.add(ch);
         }
-        return ans;
+        return true;
     }
 }
