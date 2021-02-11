76cbc9b9f84543f8db744c5f1b255b446349bb62
esh1n <eshinsergey@gmail.com>
2021-02-11 14:30:36 +0300

    added two sum with sort

---
+++ src/com/esh1n/problems/Task_167_TwoSum_SortedArray.kt
@@ -0,0 +1,37 @@
+package com.esh1n.problems
+
+object Task_167_TwoSum_SortedArray {
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val map = mapOf(intArrayOf(1,2,3,4,4,9,56,90) to 8, intArrayOf(2, 3, 4) to 6, intArrayOf(-1, 0) to -1)
+        map.forEach { (array, target) ->
+            val result = twoSum(array, target)
+            println("search of $target ${result.joinToString()}")
+        }
+    }
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        numbers.forEachIndexed { index, number ->
+            val aim = numbers.biSearch( index+1,numbers.size - 1,target - number)
+            if(aim!=-1){
+                return  intArrayOf(index+1,aim+1)
+            }
+        }
+        return intArrayOf()
+    }
+
+    private fun IntArray.biSearch(start:Int, end:Int, search:Int):Int{
+        var low = start
+        var high = end
+        while (low <= high) {
+            val pivot = (high + low) / 2
+            val med = get(pivot)
+            when {
+                med == search -> return pivot
+                search > med -> low = pivot+1
+                search < med -> high = pivot -1
+            }
+        }
+        return -1
+    }
+}
